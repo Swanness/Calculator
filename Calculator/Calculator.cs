0a7bb33926e0284c41e182c20924bd099cc15fac
Vita Lebedynska <vita.alekseevna95@gmail.com>
2023-05-31 18:35:50 +0100

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public static class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("It's not possible to divide by zero. " +
+                            "Please, pick a number above:");
+                        num2 = DealWithAZeroInput(num2);
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+        public static double DealWithAZeroInput(double num2)
+        {
+            if (!double.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Invalid input, type an integer number: ");
+                return DealWithAZeroInput(num2);
+            }
+            else
+                return num2;
+        }
+    }
+}
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+using CalculatorApp;
+using System.Threading.Channels;
+
+bool endApp = false;
+
+Console.WriteLine("Welcome to a calculator app!");
+Console.WriteLine("---------------------------");
+
+while (!endApp)
+{
+    string? numInput1 = "";
+    string? numInput2 = "";
+    double result = 0;
+
+    Console.Write("Type a number, and then press enter: ");
+    numInput1 = Console.ReadLine();
+
+    double num1 = 0;
+    while (!double.TryParse(numInput1, out num1))
+    {
+        Console.WriteLine("This is not a valid input, please enter an integer number:");
+        numInput1 = Console.ReadLine();
+    }
+
+    Console.Write("Type the second number, and then press enter: ");
+    numInput2 = Console.ReadLine();
+
+    double num2 = 0;
+    while (!double.TryParse(numInput2, out num2))
+    {
+        Console.WriteLine("This is not a valid input, please enter an integer number:");
+        numInput2 = Console.ReadLine();
+    }
+
+    Console.Write("Now, pick a correct operation: \n\ta - addition \n\ts - subtraction" +
+        "\n\tm - multiplication \n\td - division \nYour option? ");
+
+    string? op = Console.ReadLine();
+
+    try
+    {
+        result = Calculator.DoOperation(num1, num2, op);
+        while (double.IsNaN(result))
+        {
+            Console.WriteLine("The input is not valid, pick an operation from the list above: ");
+            result = Calculator.DoOperation(num1, num2, op = Console.ReadLine());
+        }
+        Console.WriteLine("The result is {0:0.##}\n", result); 
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+    }
+
+    Console.WriteLine("If you want to continue - press enter. " +
+        "If you want to close the app - press \"n\" and then enter");
+
+    if(Console.ReadLine() == "n")
+        endApp = true;
+}
+
+
+
+
